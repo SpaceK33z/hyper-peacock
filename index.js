8fac8b03de36fd4afd4b3e98a8d8a904fc4b05f2
Rutger Schimmel <rma.schimmel@gmail.com>
2016-12-12 23:08:27 +0100

    Add better tab inactive states

--- index.js
+++ index.js
@@ -1,30 +1,43 @@
+const colors = {
+  black: '#1e1d1b',
+  red: '#dc322f',
+  green: '#859900',
+  yellow: '#b58900',
+  blue: '#268bd2',
+  magenta: '#d33682',
+  cyan: '#2aa198',
+  white: '#ffffff',
+  lightBlack: '#7a7267',
+  lightRed: '#ff7c6c',
+  lightGreen: '#bcd42a',
+  lightYellow: '#e6db74',
+  lightBlue: '#26a6a6',
+  lightMagenta: '#ffa47c',
+  lightCyan: '#bcd42a',
+  lightWhite: '#f8f8f0',
+}
+
 exports.decorateConfig = (config) => {
   return Object.assign({}, config, {
     foregroundColor: '#ede0ce',
     backgroundColor: '#2b2a27',
     borderColor: '#2b2a27',
     cursorColor: 'rgba(248,28,229,0.6)',
-    colors: {
-      black: '#161a1f',
-      red: '#dc322f',
-      green: '#859900',
-      yellow: '#b58900',
-      blue: '#268bd2',
-      magenta: '#d33682',
-      cyan: '#2aa198',
-      white: '#ffffff',
-      lightBlack: '#7a7267',
-      lightRed: '#ff7c6c',
-      lightGreen: '#bcd42a',
-      lightYellow: '#e6db74',
-      lightBlue: '#26a6a6',
-      lightMagenta: '#ffa47c',
-      lightCyan: '#bcd42a',
-      lightWhite: '#f8f8f0',
-    },
-    // TODO: Don't have any useful CSS now, but there will be once I tweak it some more.
+    colors,
     css: `
       ${config.css || ''}
+      .header_header {
+        top: 0;
+        left: 0;
+        right: 0;
+      }
+      .tab_tab {
+        border-left: 0;
+        padding-left: 0;
+      }
+      .tab_tab:not(.tab_active) {
+        background: ${colors.black};
+      }
     `
   })
 }
